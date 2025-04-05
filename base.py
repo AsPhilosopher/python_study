ee3766d33465c0ba4c060d003a66160987609a69
AsPhilosopher <1003349954@qq.com>
2025-04-05 23:47:26 +0800

    added base.py

---
+++ base.py
@@ -0,0 +1,14 @@
+print("-" * 10)  # 输出10个“-”
+print(2**10)  # 幂运算
+print(9 // 2)  # 取整数
+
+price = 8.5
+weight = 7.5
+amount = price * weight
+print(amount)
+
+c = "C"
+print(c)
+print(type(c))
+print(type(2**35))
+print(type(2**100))
